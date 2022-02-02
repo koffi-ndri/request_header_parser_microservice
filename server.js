08f910cb42dd691643b6de54c0530c671965e19a
Glitch (dorian-fixed-agenda) <none>
2022-02-02 19:29:45 +0000

    🍎🚴 Checkpoint ./server.js:41771201/85

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) =>{
+  res.json({ip});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
